13e6548375b02e7ec27093a93dab2f9aa2357b28
Biplob Sutradhar <biplobsd11@gmail.com>
2022-10-07 12:48:43 +0600

    subdir hive

--- lib/core/hive/hive_init.dart
+++ lib/core/hive/hive_init.dart
@@ -1,9 +1,10 @@
+import 'package:preloadwebapptemplate/constants/string.dart';
 import 'package:preloadwebapptemplate/data/model/hive/settings.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveInit {
   static Future<void> fireUp() async {
-    await Hive.initFlutter();
+    await Hive.initFlutter(AppString.shortName);
     Hive.registerAdapter(SettingsAdapter());
   }
 }
